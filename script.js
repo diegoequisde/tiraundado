c060f7cdb998284435219bbba6f2f2c3d01b1a25
diegoequisde <diegoequisde@gmail.com>
2025-11-14 09:43:10 +0100

    añadido estilo básico y  más tipos de dados

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// Espera a que el documento esté listo
 document.addEventListener("DOMContentLoaded", () => {
-  // Obtiene referencias al botón y al párrafo
+
   const boton = document.getElementById("tirar");
+  const select = document.querySelector("select");
   const resultado = document.getElementById("resultado");
 
-  // Cuando el botón se clicke, genera un número aleatorio del 1 al 6
   boton.addEventListener("click", () => {
-    const numero = Math.floor(Math.random() * 6) + 1;
-    resultado.textContent = `🎲 Salió el número ${numero}`;
+    const caras = parseInt(select.value);
+    const numero = Math.floor(Math.random() * caras) + 1;
+
+    resultado.textContent = `🎲 ${numero}`;
   });
+
+  
 });
